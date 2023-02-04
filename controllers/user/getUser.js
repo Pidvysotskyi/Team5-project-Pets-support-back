26a1db5a8157755d9b69e5104b0a5dde6eb41455
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2023-02-04 15:54:11 +0200

    get-user

--- controllers/user/getUser.js
+++ controllers/user/getUser.js
@@ -1,7 +1,14 @@
+const { User } = require("../../models");
+const jwt = require("jsonwebtoken");
+
 const getUser = async (req, res, next) => {
-  res.json({
-    message: "get user",
-  });
+  const [Bearer, token] = req.headers.authorization.split(" ");
+
+  const { id } = jwt.decode(token);
+
+  const user = await User.findById(id);
+
+  res.status(200).json(user);
 };
 
 module.exports = getUser;
