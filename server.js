d508fe89ef99779ce0e0d9c3cb667f262f6355f0
Pidvysotskyi <Pidvysotskyi.oleksandr@gmail.com>
2023-02-04 17:03:11 +0200

    deploy

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 
-const { DB_HOST, DB_PORT } = process.env;
+const { DB_HOST, PORT } = process.env;
 
 mongoose.set("strictQuery", false);
 
 mongoose
   .connect(DB_HOST)
   .then(() =>
-    app.listen(DB_PORT, () => {
+    app.listen(PORT, () => {
       console.log("Database connection successful");
     })
   )
-  .catch((err) => {
+  .catch(err => {
     console.log(err.message);
     process.exit(1);
   });
