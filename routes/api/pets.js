dcb733fae6ceadfb3912e7f21d9337d838b9401b
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2023-02-05 09:35:55 +0200

    fix-user/pet

--- routes/api/pets.js
+++ routes/api/pets.js
@@ -4,7 +4,6 @@ const { pets: ctrl } = require("../../controllers");
 const { validation, ctrlWrapper, auth } = require("../../middlewares");
 const { newPetSchema } = require("../../schemas/");
 
-router.get("/get", auth, ctrlWrapper(ctrl.getAllPets));
 router.post("/add", auth, validation(newPetSchema), ctrlWrapper(ctrl.addPet));
 router.delete("/remove/:id", auth, ctrlWrapper(ctrl.removePet));
 
