06afc62f372fbfdb06756c18f9196c7ae61c9b53
Pidvysotskyi <Pidvysotskyi.oleksandr@gmail.com>
2023-02-07 10:10:27 +0200

    Fix patterns

--- schemas/newUser.js
+++ schemas/newUser.js
@@ -1,17 +1,26 @@
 const Joi = require("joi");
 
+const passwordError = new Error(`The password must be between 7 and 32 characters long and can contain any character except spaces`);
+const phoneError = new Error(`The phone number must be in the following format: "+38096XXXXXXX"`);
+const addressError = new Error(`The address should consist of two parts (city, region) separated by a comma and a space, like this: "Brovary, Kiev"`);
+
 const newUserSchema = Joi.object({
   password: Joi.string()
-    .min(7)
-    .max(32)
-    .pattern(/^[^ ]+$/)
+    .pattern(/^[^ ]{7,32}$/)
+    .error(passwordError)
     .required(),
   email: Joi.string().email().required(),
   name: Joi.string().required(),
-  address: Joi.string().required(),
-  phone: Joi.string()
-    .pattern(/^[0-9\-\+]{9,15}$/)
+  address: Joi.string()
+    .pattern(/^[a-zA-Z]+[,][ ][a-zA-Z]+$/)
+    .error(addressError)
     .required(),
-}).required();
+  phone: Joi.string()
+    .pattern(/^[+]{1}[0-9]{12}$/)
+    .error(phoneError)
+    .required(),
+})
+  .options({ abortEarly: false })
+  .required();
 
 module.exports = newUserSchema;
