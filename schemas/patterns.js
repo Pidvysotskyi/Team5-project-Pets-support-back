1bda9a5cb3cb899aeb41da3d9d1454d53940c7e7
Pidvysotskyi <Pidvysotskyi.oleksandr@gmail.com>
2023-02-13 10:34:17 +0200

    fix pattern

--- schemas/patterns.js
+++ schemas/patterns.js
@@ -3,7 +3,7 @@ const locationPattern = /^[a-zA-Z]+[,][ ][a-zA-Z]+$/;
 const pricePattern = /^[1-9][0-9]*$/;
 const passwordPattern = /^[^ ]{7,32}$/;
 const phonePattern = /^[+]{1}[0-9]{12}$/;
-const emailPattern = /^(([a-zA-Z0-9]{1}[a-zA-Z0-9_\-\.]{1,})@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5}))$/;
+const emailPattern = /^(([a-zA-Z0-9]{1}[a-zA-Z0-9_-.]{1,})@([a-zA-Z0-9_-.]+)\.([a-zA-Z]{2,5}))$/;
 
 module.exports = {
   allLettersPattern,
