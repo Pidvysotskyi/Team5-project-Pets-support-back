590f1c4d4c326e321f435740a72772a49fc221cf
Pidvysotskyi <Pidvysotskyi.oleksandr@gmail.com>
2023-02-15 09:23:03 +0200

    fix sort

--- controllers/notices/getAll.js
+++ controllers/notices/getAll.js
@@ -1,26 +1,21 @@
-const { Notice } = require('../../models')
-const { NotFound } = require('http-errors')
+const { Notice } = require("../../models");
+const { NotFound } = require("http-errors");
 
-const categoryList = ['lost-found', 'in-good-hands', 'sell']
+const categoryList = ["lost-found", "in-good-hands", "sell"];
 
 const getAll = async (req, res) => {
-  const { category } = req.params
-  console.log(category)
-  const { page = 1, limit = 10, keyword = '' } = req.query
-  const skip = (page - 1) * limit
+  const { category } = req.params;
+  console.log(category);
+  const { page = 1, limit = 10, keyword = "" } = req.query;
+  const skip = (page - 1) * limit;
 
   if (!categoryList.includes(category)) {
-    throw new NotFound(
-      '404, Invalid category. Your category must be one of this list: lost-found, in-good-hands, sell'
-    )
+    throw new NotFound("404, Invalid category. Your category must be one of this list: lost-found, in-good-hands, sell");
   } else {
-    if (keyword === '') {
-      const result = await Notice.find({ category }, '-createdAt -updatedAt', {
-        skip,
-        limit,
-      }).populate('owner', 'email phone')
+    if (keyword === "") {
+      const result = await Notice.find({ category }, "-createdAt -updatedAt").populate("owner", "email phone").sort({ updatedAt: "desc" }).skip(skip).limit(limit);
 
-      const notices = result.map((notice) => {
+      const notices = result.map(notice => {
         return {
           _id: notice._id,
           category: notice.category,
@@ -35,25 +30,22 @@ const getAll = async (req, res) => {
           comments: notice.comments,
           email: notice.owner.email,
           phone: notice.owner.phone,
-        }
-      })
+        };
+      });
 
       res.json({
         code: 200,
-        status: 'success',
+        status: "success",
         data: notices,
-      })
+      });
     } else {
-      const result = await Notice.find(
-        { category, title: { $regex: keyword, $options: 'i' } },
-        '-createdAt -updatedAt',
-        {
-          skip,
-          limit,
-        }
-      ).populate('owner', 'email phone')
+      const result = await Notice.find({ category, title: { $regex: keyword, $options: "i" } }, "-createdAt -updatedAt")
+        .populate("owner", "email phone")
+        .sort({ updatedAt: "desc" })
+        .skip(skip)
+        .limit(limit);
 
-      const notices = result.map((notice) => {
+      const notices = result.map(notice => {
         return {
           _id: notice._id,
           category: notice.category,
@@ -68,21 +60,21 @@ const getAll = async (req, res) => {
           comments: notice.comments,
           email: notice.owner.email,
           phone: notice.owner.phone,
-        }
-      })
+        };
+      });
 
       if (result.length === 0) {
-        throw new NotFound('404, Nothing found for your request')
+        throw new NotFound("404, Nothing found for your request");
       }
 
       res.json({
         code: 200,
-        status: 'success',
-        message: 'Get notices by category or keyword',
+        status: "success",
+        message: "Get notices by category or keyword",
         data: notices,
-      })
+      });
     }
   }
-}
+};
 
-module.exports = getAll
+module.exports = getAll;
