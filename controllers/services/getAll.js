627da068548fcd1690341fe2d90a608e46ec51be
Pidvysotskyi <Pidvysotskyi.oleksandr@gmail.com>
2023-02-17 20:06:55 +0200

    sort fiends

--- controllers/services/getAll.js
+++ controllers/services/getAll.js
@@ -1,7 +1,7 @@
 const { Service } = require("../../models");
 
 const getAll = async (req, res, next) => {
-  const Services = await Service.find();
+  const Services = await Service.find().sort({ _id: 1 });
 
   res.status(200).json(Services);
 };
